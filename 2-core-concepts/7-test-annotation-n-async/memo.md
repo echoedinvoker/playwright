3bdd2ab128465a0a1cf0c2755366c4c7677564cd
echoedinvoker <echoedinvoker@gmail.com>
2023-04-15 11:11:49 +0800

    qq

---
+++ 2-core-concepts/7-test-annotation-n-async/memo.md
@@ -0,0 +1,5 @@
+## **Create a new spec.js file**
+
+![Alt create a new spec.js file](pic/01.jpg)
+
+- This is where we actually write the test content, and then run the spec.js file through **playwright.config.js**.
---
+++ 2-core-concepts/PlayWrightAutomation/tests/UIBasicstest.spec.js
@@ -0,0 +1,8 @@
+import { test } from "@playwright/test";
+
+test("First Playwright test", async () => {
+  // playwright code --
+  // step 1 - open browser
+  // step 2 - enter username/password 2 seconds
+  // step 3 - click
+});
