f3317f87ab851fc97f100e39bdd4ae9ecb0b2d12
echoedinvoker <echoedinvoker@gmail.com>
2023-04-17 19:48:41 +0800

    qq

--- 2-core-concepts/PlayWrightAutomation/tests/MoreValidations.spec.js
+++ 2-core-concepts/PlayWrightAutomation/tests/MoreValidations.spec.js
@@ -1,14 +1,11 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("Popup test", async ({ page }) => {
-  // #displayed-text
-  // #hide-textbox
   await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-  // await expect(page.locator("#displayed-text")).toBeVisible();
-  // await page.locator("#hide-textbox").click();
-  // await expect(page.locator("#displayed-text")).toBeHidden();
 
-  await page.goto("https://google.com");
-  await page.goBack();
-  await page.goForward();
+  // #confirmbtn
+  // await page.locator("#confirmbtn").click();
+  page.on("dialog", (d) => d.accept());
+  await page.locator("#confirmbtn").click();
+  await page.pause();
 });
---
+++ 7-dialog-frame-event/30-alert-popups/memo.md
@@ -0,0 +1,31 @@
+## **NEED: confirm dialog**
+
+![Alt NEED: trigger dialog and click ok](pic/01.jpg)
+
+## **Inspecting**
+
+![Alt inspect: trigger button](pic/02.jpg)
+
+![Alt inspect: dialog](pic/03.jpg)
+
+- If we cannot inspect dialog at all, it is **java** dialog.
+
+## **Coding: page.on**
+
+> Although a java dialog does not have an element, we can control it by the event that triggers it.
+
+![Alt coding: page.on](pic/04.jpg)
+
+## **TEST**
+
+![Alt test run](pic/05.jpg)
+
+![Alt before dialog](pic/06.jpg)
+
+![Alt after dialog](pic/07.jpg)
+
+## **page.on listen event everywhere** 
+
+> Did you notice that page.on doesn't have await?
+
+![Alt coding: change event place](pic/08.jpg)
