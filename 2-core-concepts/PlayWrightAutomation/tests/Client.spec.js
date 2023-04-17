a09e8bd55b7dc216eafe90bbbc06c62530ff7a03
echoedinvoker <echoedinvoker@gmail.com>
2023-04-17 08:52:06 +0800

    qq

--- 2-core-concepts/PlayWrightAutomation/tests/Client.spec.js
+++ 2-core-concepts/PlayWrightAutomation/tests/Client.spec.js
@@ -1,11 +1,25 @@
 import { test } from "@playwright/test";
 
 test("First Playwright test", async ({ page }) => {
+  const cardBody = page.locator(".card-body");
+  const productName = "adidas original";
+
   await page.goto("https://rahulshettyacademy.com/client");
   await page.locator("#userEmail").fill("echoedinvoker@gmail.com");
   await page.locator("#userPassword").type("1234@Matt");
   await page.locator("[value='Login']").click();
 
   await page.waitForLoadState("networkidle");
-  console.log(await page.locator(".card-body b").allTextContents());
+  // console.log(await page.locator(".card-body b").allTextContents());
+
+  const count = await cardBody.count();
+  console.log(count);
+  for (let i = 0; i < count; i++) {
+    if ((await cardBody.nth(i).locator("b").textContent()) === productName) {
+      await cardBody.nth(i).locator("text=  Add To Cart").click();
+      break;
+    }
+  }
+
+  await page.pause();
 });
---
+++ 6-end-to-end/24-dynamic-find-product/memo.md
@@ -0,0 +1,21 @@
+## **NEED: find targeted product and add it to cart**
+
+![Alt NEED: find targeted product and add it to cart](pic/01.jpg)
+
+![Alt continuous codes](pic/02.jpg)
+
+## **Analyze page & elements**
+
+![Alt analyze page](pic/03.jpg)
+
+## **Coding: nested locator & loop**
+
+![Alt coding: nested locator and loop](pic/04.jpg)
+
+## **Test**
+
+![Alt run test](pic/05.jpg)
+
+![Alt pause check](pic/06.jpg)
+
+![Alt test result](pic/07.jpg)
